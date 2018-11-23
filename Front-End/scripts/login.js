adedb32803ae59a9a87be8449b31e24e9f0ec5a7
DavidJChavez <davidjchavez98@gmail.com>
2018-11-23 10:07:03 -0600

    Home added

--- Front-End/scripts/login.js
+++ Front-End/scripts/login.js
@@ -1,13 +1,4 @@
+// Tabs
 var instance = M.Tabs.init(document.getElementById("tabs-login"), { swipeable: true });
 instance.select("swipe-login");
-
-$("a").click(function (event) {
-    event.preventDefault();
-    //print(document);
-    $.post("prueba.php",
-        {
-            user: "aaa",
-            password: "bbbb"
-        },
-        console.log("Juas ya quedo"));
-});
+// End Tabs
