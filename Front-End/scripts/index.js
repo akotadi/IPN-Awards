b27f6f91dc8816e47ba0d2ae7fcd408209a592cd
DavidJChavez <davidjchavez98@gmail.com>
2018-11-28 18:18:11 -0600

    Cambios normales y .gitignore agregado

---
+++ Front-End/scripts/index.js
@@ -0,0 +1,4 @@
+// Listener para menu slide
+document.addEventListener('DOMContentLoaded', function () {
+    var instances = M.Sidenav.init(document.getElementById("slide-out"), 'left');
+});
--- Front-End/scripts/perfil.js
+++ Front-End/scripts/perfil.js
@@ -1,3 +1,14 @@
+// Listener para menu central
 document.addEventListener('DOMContentLoaded', function () {
-    var instances = M.Collapsible.init(document.getElementById("menu-collapsible"),{});
+    var instances = M.Collapsible.init(document.getElementById("menu-collapsible"), {});
+});
+
+// Listener para modal de "Eliminar cuenta"
+document.addEventListener('DOMContentLoaded', function () {
+    var instances = M.Modal.init(document.getElementById("delete-modal"), {});
+});
+
+// Listener para menu slide
+document.addEventListener('DOMContentLoaded', function () {
+    var instances = M.Sidenav.init(document.getElementById("slide-out"), 'left');
 });
